65c40307dd6b2f34950fb795b3ca5d7272a0c2fa
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2022-12-23 13:24:14 +0300

    Refactoring

--- layouts/checkout.php
+++ layouts/checkout.php
@@ -1,10 +1,10 @@
 <?php
 /*
- * @package     RadicalMart Package
+ * @package     RadicalMart Shipping Standard Plugin
  * @subpackage  plg_radicalmart_shipping_standard
- * @version     1.0.0
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
- * @copyright   Copyright (c) 2021 Delo Design. All rights reserved.
+ * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
  * @link        https://delo-design.ru/
  */
