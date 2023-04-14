531f73d7c25f15283896cecf0fd283e6fb363722
Igor Berdichevskiy <iberdichevskiy@septdir.com>
2023-04-14 15:47:11 +0300

    Reset to dev

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 /*
  * @package     RadicalMart Shipping Standard Plugin
  * @subpackage  plg_radicalmart_shipping_standard
- * @version     2.0.0
+ * @version     __DEPLOY_VERSION__
  * @author      Delo Design - delo-design.ru
  * @copyright   Copyright (c) 2022 Delo Design. All rights reserved.
  * @license     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
